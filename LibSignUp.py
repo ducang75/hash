8d0dfeb03151a8a7dbbdeac8d187e410f4474442
ducang75 <61292224+ducang75@users.noreply.github.com>
2020-02-21 06:17:01 +1100

    Update LibSignUp.py

--- LibSignUp.py
+++ LibSignUp.py
@@ -1,1 +1,9 @@
+class librariesSaved:
+    def __init__(self, set, time, numOfBook):
+
+        self.set =  set
+        self.time = time
+        self.numOfBook = numOfBook
+    def bookList(set):
+        bookSet =[];
 
